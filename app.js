63c92fc151239f1e3a6e3ca0d8c08a60b603d82d
Ameer-Hamza123 <78346308+Ameer-Hamza123@users.noreply.github.com>
2021-07-25 14:59:14 +0500

    test deploy
    updated!

--- app.js
+++ app.js
@@ -81,9 +81,6 @@ function data(){
     // console.log('profession', profession)
     document.getElementById('user_profession').innerHTML = profession
 
-    // var data = document.getElementById('education').value= localStorage.getItem('education')
-
-    // console.log('dta',data)
 var data = {
     'name' : name,
     'gender' : gender,
@@ -94,14 +91,3 @@ var data = {
 console.log(data);
 
 }
-
-function myFunction() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-  }
-
